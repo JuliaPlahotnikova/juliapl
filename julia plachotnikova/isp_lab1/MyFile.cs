777cbfb43d715c5b185d74748175469e0ae2dc3e
JuliaPlahotnikova <63021868+JuliaPlahotnikova@users.noreply.github.com>
2020-11-14 17:30:45 +0300

    Add files via upload

---
+++ julia plachotnikova/isp_lab1/MyFile.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.IO.Compression;
+
+namespace lab1
+{
+    public class MyFile
+    {
+        public void Read(string path, ArrayList list)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        list.Add(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void Write(string path, ArrayList list)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, true, System.Text.Encoding.Default))
+                {
+                    foreach (var o in list)
+                    {
+                        sw.WriteLine(o);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void BinRead(string path, ArrayList list)
+        {
+            try
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+                {
+                    // пока не достигнут конец файла
+                    // считываем каждое значение из файла
+                    while (reader.PeekChar() > -1)
+                    {
+                        list.Add(reader.ReadString());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void BinWrite(string path, ArrayList list)
+        {
+            try
+            {
+                using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+                {
+                    foreach (var o in list)
+                    {
+                        writer.Write(o as string);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void Compress(string sourceFile, string compressedFile)
+        {
+            // поток для чтения исходного файла
+            using (FileStream sourceStream = new FileStream(sourceFile, FileMode.OpenOrCreate))
+            {
+                // поток для записи сжатого файла
+                using (FileStream targetStream = File.Create(compressedFile))
+                {
+                    // поток архивации
+                    using (GZipStream compressionStream = new GZipStream(targetStream, CompressionMode.Compress))
+                    {
+                        sourceStream.CopyTo(compressionStream); // копируем байты из одного потока в другой
+                        Console.WriteLine("MyFile compression {0} completed. Original size: {1}  Compressed size: {2}.",
+                            sourceFile, sourceStream.Length.ToString(), targetStream.Length.ToString());
+                    }
+                }
+            }
+        }
+
+        public void Decompress(string compressedFile, string targetFile)
+        {
+            FileInfo fileInfo = new FileInfo(compressedFile);
+            if (fileInfo.Exists)
+            {
+                // поток для чтения из сжатого файла
+                using (FileStream sourceStream = new FileStream(compressedFile, FileMode.Open))
+                {
+                    // поток для записи восстановленного файла
+                    using (FileStream targetStream = File.Create(targetFile))
+                    {
+                        // поток разархивации
+                        using (GZipStream decompressionStream =
+                            new GZipStream(sourceStream, CompressionMode.Decompress))
+                        {
+                            decompressionStream.CopyTo(targetStream);
+                            Console.WriteLine("MyFile restored: {0}", targetFile);
+                        }
+                    }
+                }
+            }
+            else Console.WriteLine("Error. Compressed file don't exist.");
+        }
+
+        public bool IsChanged(string path)
+        {
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                var zero = TimeSpan.Zero;
+                var ctime = fileInf.LastAccessTime.Subtract(fileInf.CreationTime);
+                if (ctime > zero)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void DeleteFile(string path)
+        {
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                fileInf.Delete();
+            }
+        }
+
+        public void CopyFile(string path, string newPath, string name)
+        {
+            try
+            {
+                FileInfo fileInf = new FileInfo(path);
+                if (fileInf.Exists)
+                {
+                    if (!Directory.Exists(newPath))
+                    {
+                        Directory.CreateDirectory(newPath);
+                    }
+                    fileInf.CopyTo(newPath + name, true);
+                }
+                else Console.WriteLine("File with name {0} don't exist.", path);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+
+        public void Rename(string folder, ref string docName, string newName)
+        {
+            FileInfo fileInf = new FileInfo(folder + docName);
+            if (fileInf.Exists)
+            {
+                fileInf.MoveTo(folder + newName);
+            }
+            else Console.WriteLine("File with name {0} don't exist.", folder + docName);
+        }
+    }
+}
---
+++ julia plachotnikova/isp_lab1/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+
+
+namespace lab1
+{
+    static class Program
+    {
+        private static void Functions()
+        {
+            Console.WriteLine("1.Read from text file.");
+            Console.WriteLine("2.Write to text file.");
+            Console.WriteLine("3.Read from binary file.");
+            Console.WriteLine("4.Write to binary file.");
+            Console.WriteLine("5.Compress file.");
+            Console.WriteLine("6.Decompress file.");
+            Console.WriteLine("7.Rename file.");
+            Console.WriteLine("8.Copy file.");
+            Console.WriteLine("9.Delete file.");
+            Console.WriteLine("10.End program.");
+        }
+
+        static string NameOfFile()
+        {
+            string name = Console.ReadLine();
+            if (name != null && name.IndexOf('.') > 0)
+            {
+                name = name.Substring(0, name.IndexOf('.'));
+            }
+
+            return name;
+        }
+
+        private static void Main()
+        {
+            var folder = @"G:\JetBrains Rider 2019.3.3\USP(3sem)\";
+            var list = new ArrayList();
+            var file = new MyFile();
+            Functions();
+            var p = true;
+            while (p)
+            {
+                Console.WriteLine("Enter number of function");
+                int func;
+                while (!int.TryParse(Console.ReadLine(), out func)) Console.WriteLine("Error. Enter one more time.");
+                string doc;
+                string zip;
+                switch (func)
+                {
+                    case 1:
+                        Console.WriteLine("Enter name of text file");
+                        doc = NameOfFile() + ".txt";
+                        file.Read(folder + doc, list);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter name of text file");
+                        doc = NameOfFile() + ".txt";
+                        file.Write(folder + doc, list);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter name of binary file");
+                        doc = NameOfFile() + ".txt";
+                        file.BinRead(folder + doc, list);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter name of binary file");
+                        doc = NameOfFile() + ".txt";
+                        file.BinWrite(folder + doc, list);
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter name of text file");
+                        doc = NameOfFile() + ".txt";
+                        Console.WriteLine("Enter name of zip file");
+                        zip = NameOfFile() + ".gz";
+                        file.Compress(folder + doc, folder + zip);
+                        break;
+                    case 6:
+                        Console.WriteLine("Enter name of zip file");
+                        zip = NameOfFile() + ".gz";
+                        Console.WriteLine("Enter name of recovered file");
+                        doc = NameOfFile() + ".txt";
+                        file.Decompress(folder + zip, folder + doc);
+                        break;
+                    case 7:
+                        Console.WriteLine("Enter name of text file");
+                        doc = NameOfFile() + ".txt";
+                        Console.WriteLine("Enter new name of text file");
+                        string doc2 = NameOfFile() + ".txt";
+                        file.Rename(folder, ref doc, doc2);
+                        break;
+                    case 8:
+                        Console.WriteLine("Enter name of text file");
+                        doc = NameOfFile() + ".txt";
+                        Console.WriteLine("Enter name of folder");
+                        string folder2 = Console.ReadLine();
+                        folder2 = folder + @"\" + folder2?.Trim(Convert.ToChar(@"\"), Convert.ToChar(@"."), Convert.ToChar(@",")) + @"\";
+                        file.CopyFile(folder + doc, folder2, doc);
+                        break;
+                    case 9:
+                        Console.WriteLine("Enter name of text file");
+                        doc = NameOfFile() + ".txt";
+                        file.DeleteFile(folder + doc);
+                        break;
+                    case 10:
+                        p = false;
+                        break;
+                    default:
+                        Console.WriteLine("Error");
+                        break;
+                }
+            }
+            
+        }
+    }
+}
